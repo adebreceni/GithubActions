90f693929a8aa9658ca1e06bb561f7a81ac580eb
Adam Debreceni <57860909+adebreceni@users.noreply.github.com>
2020-07-28 09:17:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,5 @@
 #include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
 
 int main(){
-  char hostname[1024] = {};
-  gethostname(hostname, 1024);
-  std::cout << "Hostname: " << hostname << std::endl;
-  addrinfo hints;
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_CANONNAME;
-  hints.ai_protocol = 0;
-  
-  addrinfo* result = nullptr;
-  const int errcode = getaddrinfo(static_cast<const char*>(hostname), "0", &hints, &result);
-  
-  if(errcode == 0){
-    std::cout << "No error occured" << std::endl;
-  }else{
-    std::cout << "Error: " << gai_strerror(errcode) << std::endl;
-  }
+  std::cout << "Hello, World!" << std::endl;
 }
